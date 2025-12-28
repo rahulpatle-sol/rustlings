d729d43f9d2e8bc1df89e3a518f3c1a31ad2aa30
rahulpatle-sol <patler044@gmail.com>
2025-12-28 16:54:01 +0530

    function  done

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,7 +1,9 @@
 // TODO: Fix the function body without changing the signature.
 fn square(num: i32) -> i32 {
-    num * num;
+   num * num
 }
+//  removed the ; becouse ; is requirde return 
+
 
 fn main() {
     let answer = square(3);
