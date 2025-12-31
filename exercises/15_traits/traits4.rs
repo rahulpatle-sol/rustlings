46ae7b4140c67734150b5cb939ee9effc4ff8561
rahulpatle-sol <patler044@gmail.com>
2025-12-31 10:04:42 +0530

    traita

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -11,9 +11,11 @@ impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+
+fn compare_license_types<S1: Licensed, S2: Licensed>(software1: S1, software2: S2) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
+
 
 fn main() {
     // You can optionally experiment here.
