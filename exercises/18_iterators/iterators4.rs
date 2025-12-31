b20039adab93414cac52a38867ec049e83f2ce3d
rahulpatle-sol <patler044@gmail.com>
2025-12-31 14:06:20 +0530

    iterators

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -1,19 +1,9 @@
 fn factorial(num: u64) -> u64 {
-    // TODO: Complete this function to return the factorial of `num` which is
-    // defined as `1 * 2 * 3 * … * num`.
-    // https://en.wikipedia.org/wiki/Factorial
-    //
-    // Do not use:
-    // - early returns (using the `return` keyword explicitly)
-    // Try not to use:
-    // - imperative style loops (for/while)
-    // - additional variables
-    // For an extra challenge, don't use:
-    // - recursion
+    (1..=num).product()
 }
 
 fn main() {
-    // You can optionally experiment here.
+    println!("Factorial of 4 is {}", factorial(4));
 }
 
 #[cfg(test)]
@@ -29,6 +19,7 @@ mod tests {
     fn factorial_of_1() {
         assert_eq!(factorial(1), 1);
     }
+
     #[test]
     fn factorial_of_2() {
         assert_eq!(factorial(2), 2);
