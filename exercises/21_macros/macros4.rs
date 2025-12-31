6fee039b0d9f1a121257476f5bafb0969b952cdc
rahulpatle-sol <patler044@gmail.com>
2025-12-31 14:17:18 +0530

    macro  exercise done

--- exercises/21_macros/macros4.rs
+++ exercises/21_macros/macros4.rs
@@ -1,12 +1,11 @@
-// TODO: Fix the compiler error by adding one or two characters.
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    }; // ✅ add semicolon here
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
